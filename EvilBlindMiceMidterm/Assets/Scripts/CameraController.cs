1813376b4baaceaba1051e5cef076ed5a9681529
AssassinSquidGames <campbell.burger.josiah@gmail.com>
2025-09-03 22:58:56 -0500

    Added Trenton's Lecture Code

--- EvilBlindMiceMidterm/Assets/Scripts/CameraController.cs
+++ EvilBlindMiceMidterm/Assets/Scripts/CameraController.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMax, lockVertMin;
+    [SerializeField] bool invertY;
+
+    float rotX;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get input
+        float mouseX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime;
+
+        // use invertY to give options of look up/down
+        if (invertY)
+            rotX += mouseY;
+        else
+            rotX -= mouseY;
+
+        // clamp the camera on the x-axis
+        rotX = Mathf.Clamp(rotX, lockVertMax, lockVertMin);
+
+        // rotate the camera to look up and down
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        // rotate the player to look left and right
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
