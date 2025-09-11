86bd77b4e8e91160eaa92be79487980134c0559d
Nicholas Parlin <ngparlin@student.fullsail.edu>
2025-09-11 05:03:44 -0400

    minor update

--- EvilBlindMiceMidterm/Assets/Scripts/PowerUp_Trenton/SpawnPowerups.cs
+++ EvilBlindMiceMidterm/Assets/Scripts/PowerUp_Trenton/SpawnPowerups.cs
@@ -5,12 +5,14 @@ public class SpawnPowerups : MonoBehaviour
 {
     [SerializeField] List<GameObject> powerups;
     [SerializeField] int indexToSpawn;
+    GameObject powerUp;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         indexToSpawn = Mathf.Clamp(indexToSpawn, 0, powerups.Count - 1);
-        Instantiate(powerups[indexToSpawn], transform.position, Quaternion.identity);
+        powerUp = Instantiate(powerups[indexToSpawn], transform.position, Quaternion.identity);
+        powerUp.transform.localScale = new Vector3(3, 3, 3);
     }
 
     // Update is called once per frame
