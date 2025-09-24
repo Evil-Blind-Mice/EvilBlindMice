2ad1076e38c88446324541610e3afa79c3068fca
Ronnie <trulyrodarius@gmail.com>
2025-09-24 17:54:51 -0400

    FOV added to the options menu
    - Sound Manager Changed to Options Manager
    - Added FOV Slider
    - Added Functions to control FOV slider to Options Manager
    - FOV is locked between the values of 25 to 120 any higher or lower will break the camera and the game

---
+++ EvilBlindMiceMidterm/Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,70 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSider;
+    [SerializeField] Slider fovSlider;
+    [SerializeField] private Camera playerCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("gameVolume"))
+        {
+            PlayerPrefs.SetFloat("gameVolume", 0.5f);
+            LoadVolume();
+        }
+        else
+        {
+            LoadVolume();
+        }
+        if (!PlayerPrefs.HasKey("FOV"))
+        {
+            PlayerPrefs.SetFloat("FOV", 60f);
+            LoadFOV();
+        }
+        else
+        {
+            LoadFOV();
+        }
+        volumeSider.interactable = true;
+        fovSlider.interactable = true;
+    }
+    private void Update()
+    {
+       
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSider.value;
+        SaveVolume();
+    }
+    public void ChangeFOV()
+    {
+        playerCamera.fieldOfView = fovSlider.value;
+        
+        SaveFOV();
+
+    }
+    private void LoadVolume()
+    {
+        volumeSider.value = PlayerPrefs.GetFloat("gameVolume");
+        AudioListener.volume = volumeSider.value;
+    }
+    private void LoadFOV()
+    {
+        fovSlider.value = PlayerPrefs.GetFloat("FOV");
+        playerCamera.fieldOfView = fovSlider.value;
+    }
+    private void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("gameVolume", volumeSider.value);
+    }
+    private void SaveFOV()
+    {
+        PlayerPrefs.SetFloat("FOV", fovSlider.value);
+    }
+
+}
